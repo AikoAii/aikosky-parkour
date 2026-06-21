db8a1b2cee9dab44d52489f7e374703b2610a097
Nur Aiko <yaikoaiii@gmail.com>
2026-06-21 23:37:02 +0700

    Delete build directory

--- build/CMakeFiles/AikoSkyParkour.dir/src/renderer.cpp.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/AikoSkyParkour.dir/src/renderer.cpp.o: \
- /home/chikochi/test/aikosky-parkour/src/renderer.cpp \
- /usr/include/stdc-predef.h \
- /home/chikochi/test/aikosky-parkour/include/renderer.h \
- /usr/include/raylib.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/stdarg.h
